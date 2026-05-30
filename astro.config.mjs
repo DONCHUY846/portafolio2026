ef2668711ee7d7f3942610334aceab8d85a4f7f7
jesusavina-commits <jesus.avina@roii.tech>
2026-05-30 08:08:15 -0600

    feat: add background color, add icon, add titlle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import  react from '@astrojs/react';
-import glsl from 'vite-plugin-glsl';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react()],
-    vite: {
-        plugins: [glsl()]
-    }
 });
